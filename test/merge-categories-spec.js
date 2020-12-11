c173a811183484980e0b8724433a7c2f8f9e157d
Gabriel Harris <gch910@gmail.com>
2020-12-11 15:04:44 -0600

    phase 1

--- test/merge-categories-spec.js
+++ test/merge-categories-spec.js
@@ -12,14 +12,39 @@ describe("mergeCategories()", () => {
     `;
 
     it("should return no <li>s for no categories", () => {
+      let input = [];
+      let result = mergeCategories(template, input, 'li')
+      expect(result).to.contain('<div>');
+      expect(result).to.contain('</div>');
+      expect(result).to.contain('<ul>');
+      expect(result).to.contain('</ul>');
+      expect(result).to.not.contain('<li>');
+      expect(result).to.not.contain('</li>');
+      
     });
 
     it("should return a single <li> for one category", () => {
-      expect.fail('please write this test');
+      let input = ['animal'];
+      let result = mergeCategories(template, input, 'li')
+      expect(result).to.contain('<div>');
+      expect(result).to.contain('</div>');
+      expect(result).to.contain('<ul>');
+      expect(result).to.contain('</ul>');
+      expect(result).to.contain('<li>animal</li>');
+      
     });
 
     it("should return an <li> for each category", () => {
-      expect.fail('please write this test');
+      let input = ['animal', 'vehicle', 'fruits'];
+      let result = mergeCategories(template, input, 'li')
+      expect(result).to.contain('<div>');
+      expect(result).to.contain('</div>');
+      expect(result).to.contain('<ul>');
+      expect(result).to.contain('</ul>');
+      expect(result).to.contain('<li>animal</li>');
+      expect(result).to.contain('<li>vehicle</li>');
+      expect(result).to.contain('<li>fruits</li>');
+      
     });
   });
 
@@ -33,15 +58,39 @@ describe("mergeCategories()", () => {
     `;
 
     it("should return no <option>s for no categories", () => {
-      expect.fail('please write this test');
+      let input = [];
+      let result = mergeCategories(template, input, 'option')
+      expect(result).to.contain('<div>');
+      expect(result).to.contain('</div>');
+      expect(result).to.contain('<select>');
+      expect(result).to.contain('</select>');
+      expect(result).to.not.contain('<option>');
+      expect(result).to.not.contain('</option>');
+      
     });
 
     it("should return a single <option> for one category", () => {
-      expect.fail('please write this test');
+      let input = ['red paint'];
+      let result = mergeCategories(template, input, 'option')
+      expect(result).to.contain('<div>');
+      expect(result).to.contain('</div>');
+      expect(result).to.contain('<select>');
+      expect(result).to.contain('</select>');
+      expect(result).to.contain('<option>red paint</option>');
+    
+      
     });
 
     it("should return an <option> for each category", () => {
-      expect.fail('please write this test');
+      let input = ['red paint', 'blue paint', 'green paint'];
+      let result = mergeCategories(template, input, 'option')
+      expect(result).to.contain('<div>');
+      expect(result).to.contain('</div>');
+      expect(result).to.contain('<select>');
+      expect(result).to.contain('</select>');
+      expect(result).to.contain('<option>red paint</option>');
+      expect(result).to.contain('<option>blue paint</option>');
+      expect(result).to.contain('<option>green paint</option>');
     });
   });
 });
