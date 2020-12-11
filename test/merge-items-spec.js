a3147703afe5e7aec55903aefb9e122e311a3224
Gabriel Harris <gch910@gmail.com>
2020-12-11 16:12:36 -0600

    phase 3

--- test/merge-items-spec.js
+++ test/merge-items-spec.js
@@ -1,5 +1,5 @@
-const { expect } = require('chai');
-const { mergeItems } = require('../merge-items');
+const { expect } = require("chai");
+const { mergeItems } = require("../merge-items");
 describe("The mergeItems function", () => {
   const template = `
     <table>
@@ -9,18 +9,82 @@ describe("The mergeItems function", () => {
     </table>
   `;
   it("should return no <tr>s and no <td>s for no items", () => {
-    expect.fail('please write this test');
+    let items = [];
+    let result = mergeItems(template, items);
+    expect(result).to.contain("<table>");
+    expect(result).to.contain("</table>");
+    expect(result).to.contain("<tbody>");
+    expect(result).to.contain("</tbody>");
+    expect(result).to.not.contain("<tr>");
+    expect(result).to.not.contain("</tr>");
+    expect(result).to.not.contain("<td>");
+    expect(result).to.not.contain("</td>");
   });
 
   it("should return a single <tr>, four <td>s, and a <form> for one uncompleted item", () => {
-    expect.fail('please write this test');
+    const items = [
+      { title: 'Title 1', category: 'Category 1' },
+    ];
+    let result = mergeItems(template, items);
+
+    expect(result).to.contain("<table>");
+    expect(result).to.contain("</table>");
+    expect(result).to.contain("<tbody>");
+    expect(result).to.contain("</tbody>");
+    expect(result).to.contain("<tr>");
+    expect(result).to.contain("</tr>");
+    expect(result).to.contain("<td>Title 1</td>");
+    expect(result).to.contain("<td>Category 1</td>");
+    expect(result).to.contain('<form method="POST" action="/items/1">');
+    expect(result).to.not.contain( "<!-- Content here -->");
+
+    
+
+
+    
   });
 
   it("should return a single <tr>, four <td>s, and no <form> for one completed item", () => {
-    expect.fail('please write this test');
+    const items = [
+      { title: 'Title 1', category: 'Category 1', isComplete: true },
+    ];
+    let result = mergeItems(template, items);
+
+    expect(result).to.contain("<table>");
+    expect(result).to.contain("</table>");
+    expect(result).to.contain("<tbody>");
+    expect(result).to.contain("</tbody>");
+    expect(result).to.contain("<tr>");
+    expect(result).to.contain("</tr>");
+    expect(result).to.contain("<td>Title 1</td>");
+    expect(result).to.contain("<td>Category 1</td>");
+    expect(result).to.not.contain('<form method="POST" action="/items/1">');
+    expect(result).to.not.contain( "<!-- Content here -->");
+
+
   });
 
   it("should return three <tr>s for three items", () => {
-    expect.fail('please write this test');
+    const items = [
+      { title: 'Title 1', category: 'Category 1', isComplete: true },
+      { title: 'Title 2', category: 'Category 2', isComplete: true },
+      { title: 'Title 3', category: 'Category 3', isComplete: true },
+    ];
+    let result = mergeItems(template, items);
+
+    expect(result).to.contain("<table>");
+    expect(result).to.contain("</table>");
+    expect(result).to.contain("<tbody>");
+    expect(result).to.contain("</tbody>");
+    expect(result).to.contain("<tr>");
+    expect(result).to.contain("</tr>");
+    expect(result).to.contain("<td>Title 1</td>");
+    expect(result).to.contain("<td>Category 1</td>");
+    expect(result).to.contain("<td>Title 2</td>");
+    expect(result).to.contain("<td>Category 2</td>");
+    expect(result).to.contain("<td>Title 3</td>");
+    expect(result).to.contain("<td>Category 3</td>");
+    expect(result).to.not.contain('<form method="POST" action="/items/1">');
+    expect(result).to.not.contain( "<!-- Content here -->");
   });
 });
--- test/save-items-spec.js
+++ test/save-items-spec.js
@@ -2,10 +2,16 @@ const { expect } = require('chai');
 const { saveItems } = require('../save-items');
 describe("The saveItems function", () => {
   it('adds the new item to the list', () => {
-    expect.fail('please write this test');
+    const categories = ['Cat 3', 'Cat 2'];
+    const newCategory = 'Cat 1';
+    const result = saveItems(categories, newCategory);
+    expect(result).to.contain(newCategory);
+    
   });
 
   it('makes sure the result and the original are different', () => {
-    expect.fail('please write this test');
+    const categories = ['Cat 3', 'Cat 2'];
+    const result = saveItems(categories, 'Cat 1');
+    expect(result).to.not.equal(categories);
   });
 });
